8e287bfa717a539be20165bc7ff800048dbe9b3e
i科科技 <minimini0119@qq.com>
2026-01-11 18:58:41 +0800

    Add files via upload

---
+++ _middleware.js
@@ -0,0 +1,32 @@
+import { sha256 } from '../js/sha256.js';
+
+export async function onRequest(context) {
+  const { request, env, next } = context;
+  const response = await next();
+  const contentType = response.headers.get("content-type") || "";
+  
+  if (contentType.includes("text/html")) {
+    let html = await response.text();
+    
+    // 处理普通密码
+    const password = env.PASSWORD || "";
+    let passwordHash = "";
+    if (password) {
+      passwordHash = await sha256(password);
+    }
+    html = html.replace('window.__ENV__.PASSWORD = "{{PASSWORD}}";', 
+      `window.__ENV__.PASSWORD = "${passwordHash}";`);
+
+    // 移除 ADMINPASSWORD 占位符
+    html = html.replace('window.__ENV__.ADMINPASSWORD = "{{ADMINPASSWORD}}";',
+      'window.__ENV__.ADMINPASSWORD = "";');
+    
+    return new Response(html, {
+      headers: response.headers,
+      status: response.status,
+      statusText: response.statusText,
+    });
+  }
+  
+  return response;
+}
